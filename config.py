d6503a98a9aa53cc0cc6a35806834afeba7abd0c
Michel Armani <79829928+MichelArmani@users.noreply.github.com>
2026-05-04 19:44:48 -0400

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     MYSQL_HOST = os.environ.get('MYSQL_HOST', 'mysql-personal234.g.aivencloud.com')
-    MYSQL_USER = os.environ.get('MYSQL_USER', 'defaultdb')
+    MYSQL_USER = os.environ.get('MYSQL_USER', 'avnadmin')
     MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD', 'AVNS_St6kUfEfT7IczRHOJZP')
     MYSQL_DB = os.environ.get('MYSQL_DB', 'avit_db')
     MYSQL_PORT = int(os.environ.get('MYSQL_PORT', 22750))
