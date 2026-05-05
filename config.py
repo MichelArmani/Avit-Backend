5c5e3e3ec27ae1f39f0c5927e01ebc08972fbb14
Michel Armani <79829928+MichelArmani@users.noreply.github.com>
2026-05-04 21:27:07 -0400

    Update config.py

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ class Config:
     MYSQL_HOST = os.environ.get('MYSQL_HOST', 'mysql-personal234.g.aivencloud.com')
     MYSQL_USER = os.environ.get('MYSQL_USER', 'avnadmin')
     MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD', 'AVNS_St6kUfEfT7IczRHOJZP')
-    MYSQL_DB = os.environ.get('MYSQL_DB', 'avit_db')
+    MYSQL_DB = os.environ.get('MYSQL_DB', 'defaultdb')
     MYSQL_PORT = int(os.environ.get('MYSQL_PORT', 22750))
     
     # JWT Secret (sin encriptación, solo para firmar)
-    JWT_SECRET = 'avit-secret-key-2024'
+    JWT_SECRET = 'avit-secret-key-2024' 
